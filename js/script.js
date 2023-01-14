b051db71de88423c53c80d9adbc800f4732d0cb2
Monika Pflegel <monikapflegel@gmail.com>
2023-01-14 11:25:33 +0100

    changes

---
+++ js/script.js
@@ -0,0 +1,22 @@
+let amountElement = document.querySelector(".js-amount");
+let currencyElement = document.querySelector(".js-currency");
+let formElement = document.querySelector(".js-form");
+let resultElement = document.querySelector(".js-result");
+let rateEUR = 4.67;
+  let rateUSD = 4.39;
+formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
+  let amount = +amountElement.value;
+    let currency = currencyElement.value;
+    let result;
+    switch(currency){
+      case "EUR":
+        result= amount/rateEUR;
+        break;
+        case "USD":
+        result= amount/rateUSD;
+        break;
+    }
+
+ resultElement.innerText = result.toFixed(2);
+});
